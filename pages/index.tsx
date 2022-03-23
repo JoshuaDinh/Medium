934746a7da3131e976927930ec59373fb4eb6780
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-23 12:28:39 -0700

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+
+// Components
 import Navigation from "../components/Navigation";
 import Banner from "../components/Banner";
 
-const Home: NextPage = () => {
+// Types
+import { Post } from "../typings";
+
+// Functions
+import { sanityClient, urlFor } from "../sanity";
+
+interface Props {
+  posts: Post[];
+}
+
+const Home = ({ posts }: Props) => {
+  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -14,6 +27,22 @@ const Home: NextPage = () => {
       <Banner />
     </div>
   );
+};
+
+// ServerSide rendering of POSTS via Sanity.
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id, title, description,author-> {
+      name, image
+    },description,mainImage,slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
