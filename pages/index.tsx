22236a8748934dba8347337b60f2d5450cad96c7
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-23 13:08:51 -0700

    added function to loop through data and create posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-
-// Components
+import Link from "next/link";
 import Navigation from "../components/Navigation";
 import Banner from "../components/Banner";
-
-// Types
 import { Post } from "../typings";
-
-// Functions
 import { sanityClient, urlFor } from "../sanity";
 
 interface Props {
@@ -25,6 +20,34 @@ const Home = ({ posts }: Props) => {
       </Head>
       <Navigation />
       <Banner />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:gap-6 p-6 lg:grid-cols-3 gap-3 ">
+        {posts.map((post) => {
+          return (
+            <Link href={`/posts/${post.slug.current}`} key={post._id}>
+              <div className="border rounded-lg group cursor-pointer overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-lg font-bold p-1">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.title}
+                    </p>
+                  </div>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
